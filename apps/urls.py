a85c88c869a1220bc8a3eedf8fa293eafc7a40de
elbek <elbekkulmurodov01@gmail.com>
2024-06-19 12:46:09 +0500

    seller register view and url

--- apps/urls.py
+++ apps/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
 
-from apps.views import MainTemplateView
+from apps.views import MainTemplateView, LoginTemplateView, RegisterTemplateView
+from views import RegisterView, ProfileView, EmailVerificationView, PhoneVerificationView, \
+    NewUserPasswordView
 
 urlpatterns = [
     path('', MainTemplateView.as_view(), name='main_page'),
+
+    path('login/', LoginTemplateView.as_view(), name='main_page'),
+    path('register/', RegisterTemplateView.as_view(), name='main_page'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('email-verification/', EmailVerificationView.as_view(), name='verify'),
+    path('phone-verification/', PhoneVerificationView.as_view(), name='verify'),
+    path('new-user/', NewUserPasswordView.as_view(), name='verify'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+
+
 ]
--- apps/views.py
+++ apps/views.py
@@ -3,3 +3,15 @@ from django.views.generic import TemplateView
 
 class MainTemplateView(TemplateView):
     template_name = 'apps/main.html'
+
+
+class PhoneVerificationView(TemplateView):
+    template_name = 'apps/login-register/phone-number-verification.html'
+
+
+class EmailVerificationView(TemplateView):
+    template_name = 'apps/login-register/email-verification.html'
+
+
+class NewUserPasswordView(TemplateView):
+    template_name = 'apps/login-register/new-user-password.html'
