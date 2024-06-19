3a111538dd2b914ccef42bc99371b26917c75800
xolmomin <xolmomin@gmail.com>
2024-06-19 16:45:15 +0500

    change style.css

--- apps/urls.py
+++ apps/urls.py
@@ -6,7 +6,7 @@ from apps.views import RegisterPageTemplateView, EmailVerificationView, PhoneVer
 
 urlpatterns = [
     path('', MainTemplateView.as_view(), name='main_page'),
-    path('', BoxMenuTemplateView.as_view(), name='box_page'),
+    path('box-menu', BoxMenuTemplateView.as_view(), name='box_page'),
     path('', MainPageView.as_view(), name='main-page'),
     path('register/', RegisterPageTemplateView.as_view(), name='register'),
     path('email-verification/', EmailVerificationView.as_view(), name='email-verification'),
--- root/settings.py
+++ root/settings.py
@@ -54,12 +54,8 @@ AUTH_USER_MODEL = 'apps.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'beliy_bc',
-        'USER': 'postgres',
-        'PASSWORD': '1',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
