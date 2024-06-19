4a0ddd7241a66716261e9ad714fc9361d718e085
azizbek <pubgmobileazik07@gmail.com>
2024-06-19 14:43:29 +0500

    single product view

--- apps/views.py
+++ apps/views.py
@@ -1,17 +1,5 @@
 from django.views.generic import TemplateView
 
 
-class RegisterPageTemplateView(TemplateView):
-    template_name = 'apps/login-register/register.html'
-
-
-class PhoneVerificationView(TemplateView):
-    template_name = 'apps/login-register/phone-number-verification.html'
-
-
-class EmailVerificationView(TemplateView):
-    template_name = 'apps/login-register/email-verification.html'
-
-
-class NewUserPasswordView(TemplateView):
-    template_name = 'apps/login-register/new-user-password.html'
+class MainTemplateView(TemplateView):
+    template_name = 'apps/main.html'
